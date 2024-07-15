c9f91a72a240613e955471cc6e95935978728cb1
Laya0407 <layanika46@gmail.com>
2024-07-15 20:27:56 +0000

    Added route mounting and MongoDB connection setup in app.js

--- final_project/index.js
+++ final_project/index.js
@@ -3,6 +3,29 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+// app.js (or wherever your main setup is)
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/bookstore', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+// Check connection
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Include models here once defined
+require('./models/User');
+require('./models/Book');
+require('./models/Review');
 
 const app = express();
 
@@ -13,7 +36,14 @@ app.use("/customer", session({ secret: "fingerprint_customer", resave: true, sav
 app.use("/customer/auth/*", function auth(req, res, next) {
     next();
 });
+// Routes
+const booksRouter = require('./routes/books');
+const reviewsRouter = require('./routes/reviews');
+const usersRouter = require('./routes/users');
 
+app.use('/api/books', booksRouter);
+app.use('/api/reviews', reviewsRouter);
+app.use('/api/users', usersRouter);
 const PORT = 5000;
 
 app.use("/customer", customer_routes);
